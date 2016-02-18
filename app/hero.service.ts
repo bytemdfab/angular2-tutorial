e5876305581f79bea34e257ed5cefc9ca31c87ac
byte.mdfab <byte.mdfab@gmail.com>
2016-02-18 16:37:25 +0200

    Extract list of heroes to synchronous service

---
+++ app/hero.service.ts
@@ -0,0 +1,11 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
+
+@Injectable()
+
+export class HeroService {
+	getHeroes() {
+		return HEROES;
+	}
+}
