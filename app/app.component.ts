ecc990edf8f9f988e4b7afde12c6dde10cd44793
byte.mdfab <byte.mdfab@gmail.com>
2016-02-18 17:43:45 +0200

    Dashboard component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
@@ -8,6 +9,8 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 	selector: 'my-app',
 	template: `
 	<h1>{{title}}</h1>
+	<a [routerLink]="['Dashboard']">Dashboard</a>
+	<a [routerLink]="['Heroes']">Heroes</a>
 	<router-outlet></router-outlet>
 	`,
 	directives: [ROUTER_DIRECTIVES],
@@ -22,6 +25,12 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 		path: '/heroes',
 		name: 'Heroes',
 		component: HeroesComponent
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: DashboardComponent,
+		useAsDefault: true
 	}
 ])
 
