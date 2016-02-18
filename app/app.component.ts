242eff3d776a290dc2b104b89b86d6fcf4db21dd
byte.mdfab <byte.mdfab@gmail.com>
2016-02-18 17:20:15 +0200

    Add route to heroes list

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,31 @@ import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
 
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
 @Component({
 	selector: 'my-app',
 	template: `
 	<h1>{{title}}</h1>
-	<my-heroes></my-heroes>
+	<router-outlet></router-outlet>
 	`,
-	directives: [HeroesComponent],
-	providers: [HeroService]
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+		HeroService,
+		ROUTER_PROVIDERS
+	]
 })
+
+@RouteConfig([
+	{
+		path: '/heroes',
+		name: 'Heroes',
+		component: HeroesComponent
+	}
+])
 
 export class AppComponent{
 
 	title = 'Tour of heroes!';
-	
+
 }
